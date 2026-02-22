721cc3d0f6f8e6b0237d8a8372a97849501a399c
aienmii <140706899+aienmii@users.noreply.github.com>
2026-02-22 23:55:06 +0200

    _

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/wedding_inv/", 
+  base: "/wedding_invit/", 
   plugins: [react()],
   
 })
